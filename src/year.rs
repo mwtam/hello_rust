2e4f4d74e80d4a8edad1aae3f30e51441be76f81
Ming Tam <ming@fotins.com>
2023-01-19 14:43:58 +0800

    year - change to measure seconds instead of days

--- src/year.rs
+++ src/year.rs
@@ -2,30 +2,34 @@ use chrono::Datelike;
 
 
 fn main() {
-    let today = chrono::Local::now();
+    let today = chrono::Local::now().naive_local();
     // println!("{:?}", today);
 
-    let first = chrono::NaiveDate::from_ymd_opt(today.year(), 1, 1).unwrap();
+    let first = chrono::NaiveDate::
+        from_ymd_opt(today.year(), 1, 1).unwrap()
+        .and_hms_opt(0, 0, 0).unwrap();
     // println!("{:?}", first);
 
-    let next_year_first = chrono::NaiveDate::from_ymd_opt(first.year() + 1, 1, 1).unwrap();
+    // let next_year_first = chrono::NaiveDate::from_ymd_opt(first.year() + 1, 1, 1).unwrap();
+    let next_year_first = first.with_year(first.year() + 1).unwrap();
     // println!("{:?}", next_year_first);
 
     let duration_year = next_year_first - first;
-    let num_days = duration_year.num_days();
-    // println!("{:?}", num_days);
+    let num_sec = duration_year.num_seconds();
+    // println!("{:?}", num_sec);
 
-    // let diff = first - chrono::Duration::days(1);
-    let diff = today.naive_local().date() - first;
-    let days_passed = diff.num_days();
-    // println!("{:?}", days_passed);
+    let diff = today - first;
+    let sec_passed = diff.num_seconds();
+    // println!("{:?}", sec_passed);
 
-    println!("{:.2}%", 100.0 * days_passed as f64 / num_days as f64);
+    println!("{:.4}%", 100.0 * sec_passed as f64 / num_sec as f64);
 
-    for _ in 0..days_passed {
+    let percent_passed = sec_passed*100/num_sec;
+
+    for _ in 0..percent_passed {
         print!("x");
     }
-    for _ in days_passed..num_days {
+    for _ in percent_passed..100 {
         print!(".");
     }
     println!();
