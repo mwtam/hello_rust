300e84759fcdabd65986cf328629643de1d00af7
Ming Tam <ming@fotins.com>
2023-01-21 18:13:03 +0800

    infection - model virus infection

--- src/infection.rs
+++ src/infection.rs
@@ -1,29 +1,40 @@
-fn main() {
-    println!("Hello");
-    // Make a 7.5 M list of people
-    //
-    // Let groups of 3-4 closely connected (infection rate = 1)
-    // to simulate families.
-    //
-    // Let groups of 10-12 mildly connected (infection rate = 0.5)
-    // to simulate families.
-    //
-    // For each person, let them loosely connect to
-    // random 100 people (rate = 0.05) in each iteration,
-    // to simulate connections like riding on the same bus.
-    // 
-    // Remember how long they are infected.
-    // Heal them after 10 iterations.
-    // To simulate recovery
-    //
-    // Vaccine? Add a factor to decrease the rates.
-    // Say, if vaccinated, the rate is cut half.
-    // Same for those recovered.
-    //
-    // Isolation of the infected
-    // Make them very unlikely to get picked.
-    // A reject picking factor.
-    // Not re-using the vaccine factor?
+// use rand::seq::index;
 
-    // Measure how many iterations it takes to infect everyone at least once.
+fn main() {
+    use rand::{thread_rng, Rng};
+    // use rand::distributions::{Distribution, Uniform};
+
+    // let between = Uniform::from(0..7_500_000);
+    let mut rng = thread_rng();
+
+    let mut population = vec![false; 7_500_000];
+    // let mut population = vec![true; 7_500_000];
+
+    population[0] = true;
+
+    for _ in 0..20 {
+        let mut new_infection: Vec<usize> = vec![];
+        for p in &population {
+            if *p {
+                for _ in 0..10 {
+                    let n = rng.gen_range(0..population.len() as u32) as usize;
+                    // let n = between.sample(&mut rng);
+                    // let n = 10;
+                    if !population[n] {
+                        new_infection.push(n);
+                    }
+                }
+            }
+        }
+        new_infection.sort();
+        new_infection.dedup();
+
+        for i in new_infection {
+            population[i] = true;
+        }
+
+        let n = population.iter().filter(|&x| *x).count();
+        println!("n: {}", n);
+    }
+
 }
