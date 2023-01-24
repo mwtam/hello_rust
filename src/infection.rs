9f1d59dadcd87b0a0349129ad07c3f642071f3bd
Ming Tam <ming@fotins.com>
2023-01-24 12:18:16 +0800

    infection - people can recover from an infection
    And they can get infected again.

--- src/infection.rs
+++ src/infection.rs
@@ -7,18 +7,20 @@ fn main() {
     // let between = Uniform::from(0..7_500_000);
     let mut rng = thread_rng();
 
-    let mut population = vec![false; 7_500_000];
+    let infectious_round = 5;
+
+    let mut population = vec![0; 7_500_000];
     // let mut population = vec![true; 7_500_000];
 
-    population[0] = true;
+    population[0] = infectious_round;
     let n_population = population.len();
 
-    for _ in 0..20 {
+    for _ in 0..40 {
         let mut new_infection: Vec<usize> = vec![];
         new_infection.reserve(population.len());
         for (i, p) in population.iter().enumerate() {
             // if !*p {
-            //     for _ in 0..1 {
+            //     for _ in 0..0 {
             //         let n = (rng.gen_range(0..5) as usize + i) % n_population;
             //         if population[n] {
             //             new_infection.push(i);
@@ -31,29 +33,36 @@ fn main() {
             //         }
             //     }
             // }
-            if *p {
+            if *p > 0 {
                 for _ in 0..1 {
                     let n = (rng.gen_range(0..5) as usize + i) % n_population;
-                    if !population[n] {
+                    if population[n] == 0 {
                         new_infection.push(n);
                     }
                 }
                 for _ in 0..1 {
                     let n = rng.gen_range(0..n_population);
-                    if !population[n] {
+                    if population[n] == 0 {
                         new_infection.push(n);
                     }
                 }
+
+            }
+        }
+
+        for p in population.iter_mut() {
+            if *p > 0 {
+                *p -= 1;
             }
         }
         new_infection.sort();
         new_infection.dedup();
 
         for i in new_infection {
-            population[i] = true;
+            population[i] = infectious_round;
         }
 
-        let n = population.iter().filter(|&x| *x).count();
+        let n = population.iter().filter(|&x| *x > 0).count();
         println!("n: {}", n);
     }
 
