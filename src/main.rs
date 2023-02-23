b88aa46fce548741fa51ba370f0cfc08c0e743b0
mwtam <tammingwai@outlook.com>
2023-02-23 11:26:51 +0800

    Moving out of Ubuntu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use rayon::prelude::*;
 
 fn main() {
     for i in 0..4 {
@@ -14,4 +15,10 @@ fn main() {
         let j = v[1];
         println!("{i}, {j}");
     }
+
+    println!("----");
+    // let v = (0..4).combinations(2).collect::<Vec<_>>();
+    // let v = vec![[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]];
+    // v.into_par_iter();
+    // println!("{v:?}");
 }
