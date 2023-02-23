0ef1a4a2653fcca9a3694b27ff1dcbe77291372a
Ming Tam <ming@fotins.com>
2023-02-23 11:57:54 +0800

    Play with rayon more

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
-use itertools::Itertools;
 use rayon::prelude::*;
+use rand::{thread_rng, Rng, rngs::ThreadRng};
 
 fn main() {
-    for i in 0..4 {
-        for j in i+1..4 {
-            println!("{i}, {j}");
-        }
-    }
+    // for i in 0..4 {
+    //     for j in i+1..4 {
+    //         println!("{i}, {j}");
+    //     }
+    // }
 
-    println!("----");
+    // println!("----");
 
-    for v in (0..4).combinations(2) {
-        let i = v[0];
-        let j = v[1];
-        println!("{i}, {j}");
-    }
+    // for v in (0..4).combinations(2) {
+    //     let i = v[0];
+    //     let j = v[1];
+    //     println!("{i}, {j}");
+    // }
 
-    println!("----");
-    // let v = (0..4).combinations(2).collect::<Vec<_>>();
-    // let v = vec![[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]];
-    // v.into_par_iter();
-    // println!("{v:?}");
+    let mut rng = thread_rng();
+
+    let n = 6;
+    (0..n)
+    .into_par_iter()
+    .for_each(
+        |i| (i+1..n)
+        .into_par_iter()
+        .for_each(
+            |j|
+             println!("Hello {} {}", i, j)
+        )
+    );
 }
